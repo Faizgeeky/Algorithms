158fd86c004a7c08f9dfa4e7b631ba4e8d496fa0
nandoliyafaiz429@gmail.com <42071980+Destroyerfaiz@users.noreply.github.com>
2020-11-12 09:09:58 +0530

    Insertion Sort

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,1 +1,30 @@
-print("Coming soon")
+
+arr = [25,42,15,54,2,75,85,15]
+
+
+def Insertion_sort(arr):
+    for i in range(1,len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and key < arr[j] : 
+                arr[j + 1] = arr[j] 
+                j -= 1
+        arr[j + 1] = key 
+    return arr
+
+       
+       
+       
+       
+        # for j in range(i-1,0, -1):
+        #     print("J",j)
+        #     if arr[j] < cur:
+        #         arr[j+1] =arr[j]
+        #     else:
+        #         arr[j+1] = cur
+        #         break
+                
+
+if __name__ == "__main__":
+    a = Insertion_sort(arr)
+    print(a)
