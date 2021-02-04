32cae4e77bc203c5fd89a4dc94cc0ff542df9f39
nandoliyafaiz429@gmail.com <42071980+Destroyerfaiz@users.noreply.github.com>
2021-02-04 07:40:49 +0530

    Lin modified

--- LinearSearch.py
+++ LinearSearch.py
@@ -3,7 +3,12 @@
 l1 = [25,42,15,54,2,75,85,15]
 search = int(input("Enter No to search :- "))
 j = 0
+c = 0
 for i in l1:
     j = j+1
     if i == search:
+        c += 1
         print(i, " is located at position of ",j)
+    
+    if c==0:
+        print("Sorry the no you entered is not exist!")
